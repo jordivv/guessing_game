efb984962ddbdc4bf8fc2fd980d5b748ce40883e
jordivv <v.veenjordi@gmail.com>
2020-06-04 12:51:10 +1200

    initial commit

---
+++ guessing.py
@@ -0,0 +1,33 @@
+number = 45;
+print('Guess a number')
+guess = input()
+
+def higher_lower(a_guess):
+    if int(guess) > int(number):
+        print("your number is too high");
+    if int(guess) < int(number):
+        print("your number is too low")
+
+
+while guess.isdigit() == True:
+    if int(guess) == int(number):
+        print("Correct! you guessed the number")
+        break
+    else:
+        higher_lower(guess)
+        print('Guess a number')
+        guess = input()
+
+
+while guess.isdigit() != True:
+    print("please enter an integer - non integers are not allowed")
+    print('Guess a number')
+    guess = input()
+    while guess.isdigit() == True:
+        if int(guess) == int(number):
+            print("Correct! you guessed the number")
+            break
+        else:
+            higher_lower(guess)
+            print('Guess a number')
+            guess = input()
