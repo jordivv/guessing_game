9d68bf20f5bf46f3161a449871fdc51590257d74
jordivv <v.veenjordi@gmail.com>
2020-06-04 13:01:06 +1200

    comments added to the code

--- guessing.py
+++ guessing.py
@@ -1,16 +1,19 @@
+# number that the user has to guess
 number = 47;
 
+# initial instructions
 print('Guess a number')
 guess = input()
 
 
+# function determining if the number entered by the user is larger or smaller than the desired number
 def higher_lower(a_guess):
     if int(guess) > int(number):
         print("your number is too high");
     if int(guess) < int(number):
         print("your number is too low")
-        
 
+# while loop running while the entered value is a number
 while guess.isdigit() == True:
     if int(guess) == int(number):
         print("Correct! you guessed the number")
@@ -20,7 +23,7 @@ while guess.isdigit() == True:
         print('Guess a number')
         guess = input()
 
-
+# while loop running while the entered value is not a number
 while guess.isdigit() != True:
     print("please enter an integer - non integers are not allowed")
     print('Guess a number')
